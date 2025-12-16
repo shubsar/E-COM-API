b4ae4af282a3e61f8d497d9f4347068dc915d3b9
shubsar <sarkarrobin1967@gmail.com>
2025-12-16 07:44:51 +0530

    E-COM-API created

--- src/middlewares/fileupload.middleware.js
+++ src/middlewares/fileupload.middleware.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 // 2. Configure storage with filename and location.
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
-        cb(null, './uploads/');
+        cb(null, 'uploads/');
     },
     filename:(req, file, cb)=>{
         cb(null, new Date().toISOString() + file.originalname);

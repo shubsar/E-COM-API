e65e1d2b3e8f5eb098b62b63c37fe1a9136578ee
shubsar <sarkarrobin1967@gmail.com>
2025-12-17 08:01:25 +0530

    Signin signup authentication has been added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import ProductRouter from "./src/features/product/product.routes.js";
 import bodyParser from "body-parser";
+import userRouter from "./src/features/user/user.routes.js";
+
 const server = express();
 server.use(bodyParser.json());
 
 server.use("/api/products", ProductRouter);
+server.use('/api/users', userRouter);
 
 
 server.get("/", (req, res) => {
